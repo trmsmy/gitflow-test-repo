7398116da8773dfb13f5853100e216559aa4e7f1
Ramasamy, Tamil <Ramast1@LabCorp.com>
2018-03-23 17:05:27 -0400

    first commit

---
+++ src/main/java/com/lca/phoenix/gitflowtestrepo/GitflowTestRepoApplication.java
@@ -0,0 +1,12 @@
+package com.trmsmy.gitflowtestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowTestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowTestRepoApplication.class, args);
+	}
+}
